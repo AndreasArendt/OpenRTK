955be1789f4b259315716fea9962b9b74eec3937
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2023-03-12 18:12:12 +0100

    reanming obs parser

--- OpenRTK/OpenRTK.cpp
+++ OpenRTK/OpenRTK.cpp
@@ -1,19 +1,19 @@
 // OpenRTK.cpp: Definiert den Einstiegspunkt für die Anwendung.
 //
 
-#include "RinexReader/RinexObservationParser.hpp"
+#include "RinexObsParser/RinexObservationParser.hpp"
 
 using namespace std;
 
 int main()
 {
-	std::unique_ptr<RinexObservationParser> obsParser = std::make_unique<RinexObservationParser>();
+	//std::unique_ptr<RinexObservationParser> obsParser = std::make_unique<RinexObservationParser>();
 	
-	obsParser->ParseFile("D:/Projekte/OpenRTK/data/sampledata/OBE400DEU_R_20221550000_01D_30S_MO.obs");
-	//rParser->ParseFile("D:/Projekte/OpenRTK/data/sampledata/OBE400DEU_R_20230690000_01D_30S_MO.obs");	
+	//obsParser->ParseFile("D:/Projekte/OpenRTK/data/sampledata/obs/OBE400DEU_R_20221550000_01D_30S_MO.obs");
+	//rParser->ParseFile("D:/Projekte/OpenRTK/data/sampledata/obs/OBE400DEU_R_20230690000_01D_30S_MO.obs");	
 	
-	RinexEpoch x = obsParser->Epochs().front();
-	double utc = x.ConvertEpochTimeToUTC();
+	//RinexEpoch x = obsParser->Epochs().front();
+	//double utc = x.ConvertEpochTimeToUTC();
 
 
 	return 0;
