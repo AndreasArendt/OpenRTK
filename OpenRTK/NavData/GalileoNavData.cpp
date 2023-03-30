5176969a3b61efe7ea763b5f817a9162a9b34341
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2023-03-30 19:56:58 +0200

    implementation of galileo nav data

--- OpenRTK/NavData/GalileoNavData.cpp
+++ OpenRTK/NavData/GalileoNavData.cpp
@@ -12,32 +12,63 @@ GalileoNavData::~GalileoNavData()
 
 void GalileoNavData::AddClockErrors(double data0, double data1, double data2)
 {
+	_SV_ClockBias__s = data0;
+	_SV_ClockDrift__sDs = data1;
+	_SV_ClockDriftRate__sDs2 = data2;
 }
 
 void GalileoNavData::AddOrbit_1(double data0, double data1, double data2, double data3)
 {
+	_IODE_IssueOfData = data0;
+	_Crs__m = data1;
+	_DeltaN__radDs = data2;
+	_M0__rad = data3;
 }
 
 void GalileoNavData::AddOrbit_2(double data0, double data1, double data2, double data3)
 {
+	_Cuc__rad = data0;
+	_Eccentricity = data1;
+	_Cus__rad = data2;
+	_SqrtA___sqrtm = data3;
 }
 
 void GalileoNavData::AddOrbit_3(double data0, double data1, double data2, double data3)
 {
+	_Toe__s = data0;
+	_Cic__rad = data1;
+	_Omega0__rad = data2;
+	_Cis__rad = data3;
 }
 
 void GalileoNavData::AddOrbit_4(double data0, double data1, double data2, double data3)
 {
+	_i0__rad = data0;
+	_Crc__m = data1;
+	_Omega__rad = data2;
+	_Omega_dot__radDs = data3;
 }
 
 void GalileoNavData::AddOrbit_5(double data0, double data1, double data2, double data3)
 {
+	_Idot__radDs = data0;
+	_DataSources = data1;
+	_GalWeek = data2;
+	_Spare0 = 0.0;
 }
 
 void GalileoNavData::AddOrbit_6(double data0, double data1, double data2, double data3)
 {
+	_SigAccuracy__m = data0;
+	_SvHealth = data1;
+	_BDG_E5a_E1 = data2;
+	_BDG_E5b_E1 = data3;
 }
 
 void GalileoNavData::AddOrbit_7(double data0, double data1, double data2, double data3)
 {
+	_TransmissiontimeOfMessage = data0;
+	_Spare1 = 0.0;
+	_Spare2 = 0.0;
+	_Spare3 = 0.0;
 }
--- OpenRTK/NavData/GalileoNavData.hpp
+++ OpenRTK/NavData/GalileoNavData.hpp
@@ -5,6 +5,53 @@
 class GalileoNavData : virtual public NavData
 {
 private:
+	// Clock Data
+	double _SV_ClockBias__s = 0.0;
+	double _SV_ClockDrift__sDs = 0.0;
+	double _SV_ClockDriftRate__sDs2 = 0.0;
+
+	//Orbit 1
+	double _IODE_IssueOfData = 0.0;
+	double _Crs__m = 0.0;
+	double _DeltaN__radDs = 0.0;
+	double _M0__rad = 0.0;
+
+	// Orbit 2
+	double _Cuc__rad = 0.0;
+	double _Eccentricity = 0.0;
+	double _Cus__rad = 0.0;
+	double _SqrtA___sqrtm = 0.0;
+	
+	// Orbit 3
+	double _Toe__s = 0.0; //Time of Ephemeris
+	double _Cic__rad = 0.0;
+	double _Omega0__rad = 0.0;
+	double _Cis__rad = 0.0;
+
+	// Orbit 4
+	double _i0__rad = 0.0;
+	double _Crc__m = 0.0;
+	double _Omega__rad = 0.0;
+	double _Omega_dot__radDs = 0.0;
+
+	// Orbit 5
+	double _Idot__radDs = 0.0;
+	double _DataSources = 0.0;
+	double _GalWeek = 0.0;
+	double _Spare0 = 0.0;
+
+	// Orbit 6
+	double _SigAccuracy__m = 0.0;
+	double _SvHealth = 0.0;
+	double _BDG_E5a_E1 = 0.0;
+	double _BDG_E5b_E1 = 0.0;
+
+	// Orbit 7		
+	double _TransmissiontimeOfMessage = 0.0;
+	double _Spare1 = 0.0;
+	double _Spare2 = 0.0;
+	double _Spare3 = 0.0;
+
 public:
 	// Geerbt über NavData
 	void AddClockErrors(double data0, double data1, double data2);
