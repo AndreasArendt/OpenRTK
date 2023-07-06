01bc83630dfd163c226d0de52b934d43fdab21c8
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2023-07-06 21:54:20 +0200

    bugfix

---
+++ OpenRTK/Transformations/Transformation.cpp
@@ -0,0 +1,5 @@
+#include "Transformation.hpp"
+
+const double Transformation::GravitationalConstant__m3Ds2 = 3.986004418e14;
+const double Transformation::MeanAngularVelocityOfEarth__radDs = 7.2921151467e10 - 5;
+const double Transformation::SpeedOfLight__mDs = 299792458;
--- OpenRTK/Transformations/Transformation.hpp
+++ OpenRTK/Transformations/Transformation.hpp
@@ -10,7 +10,3 @@ public:
 	static const double MeanAngularVelocityOfEarth__radDs;
 	static const double SpeedOfLight__mDs;
 };
-
-const double Transformation::GravitationalConstant__m3Ds2 = 3.986004418e14;
-const double Transformation::MeanAngularVelocityOfEarth__radDs = 7.2921151467e10 - 5;
-const double Transformation::SpeedOfLight__mDs = 299792458;
