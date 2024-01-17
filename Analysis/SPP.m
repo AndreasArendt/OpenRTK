3e444d1c9902fe1121e4c59110077023769a1a91
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2024-01-17 21:23:43 +0100

    update spp - currently not best performance with estimated dtr

--- Analysis/SPP.m
+++ Analysis/SPP.m
@@ -4,9 +4,6 @@ idx_E    = ([S.SvSystemID_t_eph] >= 17664) & ([S.SvSystemID_t_eph] <= 17664+255)
 SvId_E   = unique(S.SvSystemID_t_eph(idx_E));
 number_E = numel(unique(S.SvSystemID_t_eph(idx_E)));
 
-rho = S.Code_1(idx_E);
-
-Theta = [0;0;0];
 pos = [0;0;0];
 cdtr = 0;
 
@@ -24,11 +21,15 @@ for tt = timestamps(1:180).'
                        (S.Code_1 > 1e3) & (S.Code_1 > 1e5) & ...                % valid Pseudoranges
                        ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
     code_1_idx = code_idx & (S.Band == 1);
-
+    code_5_idx = code_idx & (S.Band == 5);
+    
+    [ia,ib] = ismember(S.SvSystemID_t_eph(code_1_idx), S.SvSystemID_t_eph(code_5_idx));
+    tmp = find(code_1_idx);
+    code_1_idx(tmp(~ia)) = 0;
     
     POS = pos(:,ctr);
-
-    for ii = 1:10
+    CDTR = cdtr(ctr);
+    for ii = 1:10       
         [r, d, e] = CalcDistance( S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
                                   POS(1), POS(2), POS(3));
     
@@ -40,23 +41,42 @@ for tt = timestamps(1:180).'
     
         % delta_pseudorange_1 = S.Code_1(code_1_idx) - cdtr(ctr) + cdt_sv - tropo_offset - cdt_rel;
         % delta_pseudorange_1 = S.Code_1(code_1_idx) + cdt_sv - tropo_offset - cdt_rel;
-        delta_pseudorange_1 = S.Code_1(code_1_idx) + cdt_sv - cdt_rel;
         
-        % A = [-e, ones(numel(e(:,1)),1)];
-        A = -e;
-        y = delta_pseudorange_1 - r;
+         rho_iono_free = ((F_E1_Galileo__Hz^2 * S.Code_1(code_1_idx)) - (F_E5a_Galileo__Hz^2 * S.Code_5(code_5_idx))) ./ ...
+            (F_E1_Galileo__Hz^2 - F_E5a_Galileo__Hz^2);
+        
+        delta_pseudorange_1 = rho_iono_free + cdt_sv - cdt_rel;
+        
+        A = [-e, ones(numel(e(:,1)),1)];
+        % A = -e;
+
+        dist = r + CDTR;
+
+        y = delta_pseudorange_1 - dist;
     
+        [~, idx_max] = max(abs(y));
+
+        if numel(y) > 4
+            idx_y = 1:numel(y);
+            idx_y = idx_y ~= idx_max;
+
+            A = A(idx_y,:);
+            y = y(idx_y);
+        end
+
         x_hat = (A.'*A)\A.'*y;
         
-        POS = POS + x_hat;        
+        POS = POS + x_hat(1:3);        
+        CDTR = CDTR + x_hat(4);
 
-        if all(x_hat < 1e-3)
+        if all(x_hat(1:3) < 1e-3)
             break;
         end
     end
         
     ctr = ctr+1;
     pos(:,ctr) = POS;
+    cdtr(ctr) = CDTR;
 end
 
 %%
