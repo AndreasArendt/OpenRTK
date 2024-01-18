2a6fdc734b6eaa4a60bfefb278daca69f96d1e2c
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2024-01-18 21:36:08 +0100

    massivly improve runtime

--- Analysis/SPP.m
+++ Analysis/SPP.m
@@ -1,5 +1,7 @@
 % LoadData;
 
+profile on;
+
 idx_E    = ([S.SvSystemID_t_eph] >= 17664) & ([S.SvSystemID_t_eph] <= 17664+255); %17664 is 69 ('E') << 8
 SvId_E   = unique(S.SvSystemID_t_eph(idx_E));
 number_E = numel(unique(S.SvSystemID_t_eph(idx_E)));
@@ -9,15 +11,17 @@ cdtr = 0;
 
 ctr = 1;
 
+idx_Galileo = cellfun(@(x)strcmp(x(1), 'E'), S.SvSystem_t_eph);
+
 timestamps = unique(S.ObsToc);
-for tt = timestamps(1:180).'   
+for tt = timestamps.'   
     idx = S.ObsToc == tt;
 
     % filter out unhealthy satellites    
     idx = idx & S.Health == 0;
     
     % Indices
-    code_idx   = idx & cellfun(@(x)strcmp(x(1), 'E'), S.SvSystem_t_eph) & ...   % Galileo only!
+    code_idx   = idx & idx_Galileo & ...   % Galileo only!
                        (S.Code_1 > 1e3) & (S.Code_1 > 1e5) & ...                % valid Pseudoranges
                        ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
     code_1_idx = code_idx & (S.Band == 1);
@@ -79,31 +83,39 @@ for tt = timestamps(1:180).'
     cdtr(ctr) = CDTR;
 end
 
+profile viewer;
+
 %%
 afigure(42);
 subplot(3,2,1);
 hold on;
 yline(station_pos__m(1), 'Color', Color.BLACK)
 plot(pos(1,:))
+ylim([station_pos__m(1)-50,  station_pos__m(1)+50])
 
 subplot(3,2,2);
 hold on;
 plot(station_pos__m(1) - pos(1,:))
+ylim([-20 20])
 
 subplot(3,2,3);
 hold on;
 yline(station_pos__m(2), 'Color', Color.BLACK)
 plot(pos(2,:))
+ylim([station_pos__m(2)-50, station_pos__m(2)+50])
 
 subplot(3,2,4);
 hold on;
 plot(station_pos__m(2) - pos(2,:))
+ylim([-20 20])
 
 subplot(3,2,5);
 hold on;
 yline(station_pos__m(3), 'Color', Color.BLACK)
 plot(pos(3,:))
+ylim([station_pos__m(3)-50, station_pos__m(3)+50])
 
 subplot(3,2,6);
 hold on;
 plot(station_pos__m(3) - pos(3,:))
+ylim([-20 20])
