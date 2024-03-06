0a3cf896c65e050058ef23ed64289782ff84a8ed
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2024-03-06 21:50:34 +0100

    update spp

--- Analysis/SPP.m
+++ Analysis/SPP.m
@@ -20,7 +20,7 @@ for tt = timestamps.'
     
     % Indices
     code_idx   = idx & idx_Galileo & ...   % Galileo only!
-                       (S.Code_1 > 1e3) & (S.Code_1 > 1e5) & ...                % valid Pseudoranges
+                       (S.Code_1 > 1e3) & (S.Code_1 > 1e3) & ...                % valid Pseudoranges
                        ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
     code_1_idx = code_idx & (S.Band == 1);
     code_5_idx = code_idx & (S.Band == 5);
@@ -31,7 +31,8 @@ for tt = timestamps.'
     
     POS = pos(:,ctr);
     CDTR = cdtr(ctr);
-    for ii = 1:10       
+    
+    for ii = 1:10           
         [r, d, e] = CalcDistance( S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
                                   POS(1), POS(2), POS(3));
     
@@ -40,32 +41,17 @@ for tt = timestamps.'
     
         tropo_offset = CalcTropoOffset(POS(1), POS(2), POS(3), ...
                                        d(:,1), d(:,2), d(:,3));
-    
-        % delta_pseudorange_1 = S.Code_1(code_1_idx) - cdtr(ctr) + cdt_sv - tropo_offset - cdt_rel;
-        % delta_pseudorange_1 = S.Code_1(code_1_idx) + cdt_sv - tropo_offset - cdt_rel;
-        
-         rho_iono_free = ((F_E1_Galileo__Hz^2 * S.Code_1(code_1_idx)) - (F_E5a_Galileo__Hz^2 * S.Code_5(code_5_idx))) ./ ...
+            
+        rho_iono_free = ((F_E1_Galileo__Hz^2 * S.Code_1(code_1_idx)) - (F_E5a_Galileo__Hz^2 * S.Code_5(code_5_idx))) ./ ...
             (F_E1_Galileo__Hz^2 - F_E5a_Galileo__Hz^2);
         
-        delta_pseudorange_1 = rho_iono_free + cdt_sv - cdt_rel;
+        pseudorange_est = rho_iono_free + cdt_sv + cdt_rel - CDTR - tropo_offset;        
         
         A = [-e, ones(numel(e(:,1)),1)];
         % A = -e;
 
-        dist = r + CDTR;
-
-        y = delta_pseudorange_1 - dist;
-    
-        [~, idx_max] = max(abs(y));
-
-        if numel(y) > 4
-            idx_y = 1:numel(y);
-            idx_y = idx_y ~= idx_max;
-
-            A = A(idx_y,:);
-            y = y(idx_y);
-        end
-
+        y = pseudorange_est - r;
+          
         x_hat = (A.'*A)\A.'*y;
         
         POS = POS + x_hat(1:3);        
