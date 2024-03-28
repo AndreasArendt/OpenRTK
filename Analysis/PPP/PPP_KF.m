3217c94a7602b15a7528a9a5e568c61caab0c03a
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2024-03-28 21:54:08 +0100

    prepare ppp algorithm

---
+++ Analysis/PPP/PPP_KF.m
@@ -0,0 +1,206 @@
+% LoadData;
+
+idx_E    = ([S.SvSystemID_t_eph] >= 17664) & ([S.SvSystemID_t_eph] <= 17664+255); %17664 is 69 ('E') << 8
+SvId_E   = unique(S.SvSystemID_t_eph(idx_E));
+number_E = numel(unique(S.SvSystemID_t_eph(idx_E)));
+
+ctr = 1;
+
+idx_Galileo = cellfun(@(x)strcmp(x(1), 'E'), S.SvSystem_t_eph);
+idx_GPS     = cellfun(@(x)strcmp(x(1), 'G'), S.SvSystem_t_eph) .* 0;
+
+idx_SV = idx_Galileo | idx_GPS;
+
+nAmbiguity = numel(unique(S.SvSystemID_t_eph(idx_SV)));
+
+% KF Initialization
+nStates = 8 + nAmbiguity;
+F = eye(nStates);
+F(1:3, 4:6) = eye(3) .* 30; % I * dt
+
+Q_pos = [0.01 0.01 0.01];
+Q_vel = [0.01 0.01 0.01];
+Q_cdt = 1e-3;
+Q_ztd = 1e-9;
+Q_N   = 0.01 .* ones(1, nAmbiguity); % Ambiguities
+
+Q = diag([Q_pos, Q_vel, Q_cdt, Q_ztd, Q_N]);
+kf = Kalman.Kalman(F, Q);
+kf.SetInitialStates([station_pos__m'; zeros(3,1); 0; 0; zeros(nAmbiguity,1)]);    % ECEF Position [m]
+                                                                                  % ECEF Velocity [m]
+                                                                                  % Receiver Clock Error * c [m]
+                                                                                  % Zenith Troposphere Delay [m]
+                                                                                  % Ambiguities
+
+pos = station_pos__m;
+cdr = 0;
+zdt = 0;
+Ambiguity = zeros(nAmbiguity,1);
+P = eye(nStates);
+
+P_pri  = eye(nStates);
+P_post = eye(nStates);
+X_pri  = kf.X.';
+X_post = kf.X.';
+
+KfIterations = 1;
+
+timestamps = unique(S.ObsToc);
+for tt = timestamps.'   
+    idx = S.ObsToc == tt;
+
+    kf.Predict();
+
+    X_pri(ctr,:)   = kf.X.';
+    P_pri(:,:,ctr) = kf.P;
+
+    % filter out unhealthy satellites    
+    idx = idx & S.Health == 0;
+    
+    % Indices
+    code_idx   = idx & (idx_Galileo | idx_GPS) & ...
+                       (S.Code_1 > 1e3) & (S.Code_1 > 1e3) & ...                % valid Pseudoranges
+                       ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
+    code_1_idx = code_idx & (S.Band == 1);
+    code_5_idx = code_idx & (S.Band == 5);
+    
+    phase_idx   = idx & (idx_Galileo | idx_GPS) & ...
+                       (S.Phase_1 > 0) & (S.Code_5 > 0) & ...                   % valid Carrierphases
+                       ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
+    phase_1_idx = code_idx & (S.Band == 1);
+    phase_5_idx = code_idx & (S.Band == 5);
+
+    [ia,ib] = ismember(S.SvSystemID_t_eph(code_1_idx), S.SvSystemID_t_eph(code_5_idx));
+    tmp = find(code_1_idx);
+    code_1_idx(tmp(~ia)) = 0;
+    
+    [ia,ib] = ismember(S.SvSystemID_t_eph(phase_1_idx), S.SvSystemID_t_eph(phase_5_idx));
+    tmp = find(phase_1_idx);
+    phase_1_idx(tmp(~ia)) = 0;
+
+    for ii = 1:KfIterations
+        POS   = kf.X(1:3);
+        VEL   = kf.X(4:6);
+        CDTR  = kf.X(7);
+        ZTD   = kf.X(8);
+        Ambig = kf.X(9:end);
+
+        % Calc Distance 
+        dist = Vector.EuclidianDistance_3D(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
+                                  POS(1), POS(2), POS(3));
+
+        % Sagnac Correction
+        sag = Transformation.CalcSagnac( [S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx)], ...
+                                         [POS(1), POS(2), POS(3)]);
+
+        geo_dist = dist + sag;
+
+        % distance Vector & normalized distance Vector
+        d = Vector.DistanceVector_3D(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
+                                     POS(1), POS(2), POS(3));
+
+        e = Vector.NormalizedDistanceVector(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
+                                            POS(1), POS(2), POS(3));
+
+        cdt_sv  = Transformation.SpeedOfLight__mDs * S.SvClockOffset(code_1_idx);    
+        cdt_rel = Transformation.SpeedOfLight__mDs * S.RelativisticError(code_1_idx);
+    
+        % elevation mask
+        elevation = Generic.CalcElevation(POS(1), POS(2), POS(3), d(:,1), d(:,2), d(:,3));
+        
+        idx_el = elevation > (15/180*pi) & elevation < (165/180*pi);    
+        [lat__rad, lon__rad, alt__m] = Transformation.ecef2wgs84(POS(1), POS(2), POS(3));
+    
+        d_dry = 2.3 .* exp(-0.116e-3 .* alt__m);
+    
+        if isinf(d_dry)
+            d_dry = 1e9;
+        end
+        
+        d_wet = 0.1 + ZTD;        
+        M_E = 1.001./sqrt(0.002001 + sin(elevation).^2);                        
+        tropo_offset = (d_dry + d_wet) .* M_E;
+               
+        % Iono-Free LC
+        rho_iono_free = Generic.CalcIonoFreeLinearCombination(S.Code_1(code_1_idx), S.Code_5(code_5_idx), F_E1_Galileo__Hz, F_E5a_Galileo__Hz);
+             
+        pseudorange_est = rho_iono_free + cdt_sv + cdt_rel - CDTR - tropo_offset;        
+        
+        nObs = numel(e(:,1));
+        H = [-e, zeros(nObs, 3), ones(nObs,1), M_E, zeros(nObs, nAmbiguity)];
+        dy = pseudorange_est - geo_dist;
+    
+        if ii < KfIterations
+            kf.CorrectIterativeEKF(H, dy, eye(numel(dy)));
+        else
+            kf.CorrectEKF(H, dy, eye(numel(dy)));
+        end
+    end
+
+    pos(ctr,:) = kf.X(1:3);
+    vel(ctr,:) = kf.X(4:6);
+    cdr(ctr,:) = kf.X(7);
+    ztd(ctr,:) = kf.X(8);    
+    P(ctr,:) = diag(kf.P)';
+
+    % for RTS
+    X_post(ctr,:) = kf.X.';
+    P_post(:,:,ctr) = kf.P;
+
+    ctr = ctr+1;
+end
+         
+% ReadRtkPos;
+% return;
+%%
+afigure(42);
+subplot(4,2,1);
+hold on; grid on;
+yline(station_pos__m(1), 'Color', Color.BLACK, 'HandleVisibility','off')
+plot(pos(:,1), 'DisplayName', 'EKF (5 States)')
+ylim([station_pos__m(1)-50,  station_pos__m(1)+50])
+legend('show')
+
+subplot(4,2,2);
+hold on; grid on;
+plot(station_pos__m(1) - pos(:,1), 'DisplayName', 'EKF (5 States)')
+ylim([-20 20])
+legend('show')
+
+subplot(4,2,3);
+hold on; grid on;
+yline(station_pos__m(2), 'Color', Color.BLACK, 'HandleVisibility','off')
+plot(pos(:,2), 'DisplayName', 'EKF (5 States)')
+ylim([station_pos__m(2)-50, station_pos__m(2)+50])
+legend('show')
+
+subplot(4,2,4);
+hold on; grid on;
+plot(station_pos__m(2) - pos(:,2), 'DisplayName', 'EKF (5 States)')
+ylim([-20 20])
+legend('show')
+
+subplot(4,2,5);
+hold on; grid on;
+yline(station_pos__m(3), 'Color', Color.BLACK, 'HandleVisibility','off')
+plot(pos(:,3), 'DisplayName', 'EKF (5 States)')
+ylim([station_pos__m(3)-50, station_pos__m(3)+50])
+legend('show')
+
+subplot(4,2,6);
+hold on; grid on;
+plot(station_pos__m(3) - pos(:,3), 'DisplayName', 'EKF (5 States)')
+ylim([-20 20])
+legend('show')
+
+subplot(4,2,7);
+hold on; grid on;
+title('dt_r')
+plot(cdr ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'EKF (5 States)')
+legend('show')
+
+subplot(4,2,8);
+hold on; grid on;
+title('ZTD_w')
+plot(ztd, 'DisplayName', 'EKF (5 States)')
+legend('show')
--- Analysis/init.m
+++ Analysis/init.m
@@ -2,3 +2,4 @@ BasePath = fileparts(mfilename("fullpath"));
 addpath(genpath(fullfile(BasePath, 'Misc')))
 addpath(genpath(fullfile(BasePath, 'Utils')))
 addpath(genpath(fullfile(BasePath, 'SPP')))
+addpath(genpath(fullfile(BasePath, 'PPP')))
