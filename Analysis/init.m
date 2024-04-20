0fe8a9ef5b724f7e7a173c60580d7da750103378
DESKTOP-J1HDBS3\andia <andi.arendt@icloud.com>
2024-04-20 17:55:20 +0200

    first draft of PPP

--- Analysis/init.m
+++ Analysis/init.m
@@ -3,3 +3,5 @@ addpath(genpath(fullfile(BasePath, 'Misc')))
 addpath(genpath(fullfile(BasePath, 'Utils')))
 addpath(genpath(fullfile(BasePath, 'SPP')))
 addpath(genpath(fullfile(BasePath, 'PPP')))
+
+LoadData;
