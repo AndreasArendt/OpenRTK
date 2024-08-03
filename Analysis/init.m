aff88dc7e7fe9ec2a308a275515dc7703e1ae992
aarendt <andi.arendt@icloud.com>
2024-08-03 11:29:40 +0200

    update init function

--- Analysis/init.m
+++ Analysis/init.m
@@ -1,7 +1,7 @@
 BasePath = fileparts(mfilename("fullpath"));
 addpath(genpath(fullfile(BasePath, 'Misc')))
-addpath(genpath(fullfile(BasePath, 'Utils')))
 addpath(genpath(fullfile(BasePath, 'SPP')))
 addpath(genpath(fullfile(BasePath, 'PPP')))
+addpath(genpath(fullfile(fileparts(BasePath), 'submodules', 'utils', 'matlab')))
 
 LoadData;
