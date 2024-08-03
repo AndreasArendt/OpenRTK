043fc68fe9d959e5c46f3e86a150cb211b0d8462
aarendt <andi.arendt@icloud.com>
2024-08-03 11:28:53 +0200

    move utils

--- Analysis/Utils/@Generic/CalcElevation.m
+++
@@ -1,7 +0,0 @@
-function elevation__rad = CalcElevation(x_rx, y_rx, z_rx, dx, dy, dz)
-
-    [lat__rad, lon__rad, ~] = Transformation.ecef2wgs84(x_rx, y_rx, z_rx);                
-    [E,N,U] = Transformation.ecef2enu(lat__rad, lon__rad, dx, dy, dz);
-    
-    elevation__rad = atan2(U, sqrt(E.^2 + N.^2));    
-end
