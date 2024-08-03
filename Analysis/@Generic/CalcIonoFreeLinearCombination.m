5abbce7fe9edb1a178ffba4e8067356b85d4ebd1
aarendt <andi.arendt@icloud.com>
2024-08-03 11:29:57 +0200

    move generics

---
+++ Analysis/@Generic/CalcIonoFreeLinearCombination.m
@@ -0,0 +1,7 @@
+function [r_ionofree] = CalcIonoFreeLinearCombination(x_1, x_2, F1, F2)
+%CalcIonoFreeLinearCombination Summary of this function goes here
+%   Detailed explanation goes here
+
+    r_ionofree = ((F1^2 .* x_1) - (F2^2 .* x_2)) ./ (F1^2 - F2^2);
+end
+
