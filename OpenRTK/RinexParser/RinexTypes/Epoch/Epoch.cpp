2fffe43fee42c8528e3a89031994d7c2097d7dca
aarendt <andi.arendt@icloud.com>
2024-08-07 21:57:32 +0200

    minor cleanup with epoch time

--- OpenRTK/RinexParser/RinexTypes/Epoch/Epoch.cpp
+++ OpenRTK/RinexParser/RinexTypes/Epoch/Epoch.cpp
@@ -8,6 +8,8 @@ Epoch::Epoch()
     this->_hour = 0;
     this->_minute = 0;
     this->_second = 0.0;
+
+    this->_EpochTime = std::chrono::system_clock::time_point::min();
 }
 
 Epoch::Epoch(int year, int month, int day, int hour, int minute, double second)    
@@ -16,17 +18,18 @@ Epoch::Epoch(int year, int month, int day, int hour, int minute, double second)
     int epochSeconds = static_cast<int>(second);
     
     std::tm timeinfo{}; // initialize timeinfo
-    timeinfo.tm_year = year - 1900; // tm_year is the year since 1900
+    timeinfo.tm_year = year - 1900; // tm_year  is the year since 1900
     timeinfo.tm_mon = month - 1; // tm_mon is the month (0-11)
     timeinfo.tm_mday = day; // tm_mday is the day of the month (1-31)
     timeinfo.tm_hour = hour + 1; // tm_hour is the hour (0-23) -1 -> Rinex Standard has range from (1-24)
     timeinfo.tm_min = minute; // tm_min is the minute (0-59)
     timeinfo.tm_sec = second; // tm_sec is the second (0-60)
     
-    std::time_t timeSinceEpoch = std::mktime(&timeinfo);
+    std::chrono::duration<double> milliseconds = std::chrono::duration<double>(second - epochSeconds);
 
+    std::time_t timeSinceEpoch = std::mktime(&timeinfo);
     this->_EpochTime = std::chrono::system_clock::from_time_t(timeSinceEpoch);
-    this->_EpochTime += std::chrono::milliseconds( static_cast<int>((second -static_cast<double>(epochSeconds)) * 1e3) );
+    this->_EpochTime += std::chrono::duration_cast<std::chrono::milliseconds>(milliseconds);
 
     this->_year = year;
     this->_month = month;
