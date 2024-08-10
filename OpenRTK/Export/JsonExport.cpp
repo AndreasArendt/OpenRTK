150ba3c47cec03338beedb547bd9d93c7d7f95f4
aarendt <andi.arendt@icloud.com>
2024-08-10 10:26:04 +0200

    bugfixing in json obsdata creation

--- OpenRTK/Export/JsonExport.cpp
+++ OpenRTK/Export/JsonExport.cpp
@@ -4,35 +4,76 @@
 
 using json = nlohmann::json;
 
-SatelliteObservation JsonExport::CreateSatObservation(Satellite sv, ObsData obs, const Ephemeris* ephemeris)
+double JsonExport::GetCodeObservationIfExist(ObsData obs, ObservationBand band)
+{
+	if (obs.CodeObservations().find(band) != obs.CodeObservations().end())
+	{
+		return obs.CodeObservations().at(band).Pseudorange__m();
+	}	
+
+	return 0;
+}
+
+double JsonExport::GetPhaseObservationIfExist(ObsData obs, ObservationBand band)
+{
+	if (obs.PhaseObservations().find(band) != obs.PhaseObservations().end())
+	{
+		return obs.PhaseObservations().at(band).Carrierphase__Cycles();
+	}
+
+	return 0;
+}
+
+double JsonExport::GetDopplerObservationIfExist(ObsData obs, ObservationBand band)
+{
+	if (obs.DopplerObservations().find(band) != obs.DopplerObservations().end())
+	{
+		return obs.DopplerObservations().at(band).Doppler__Hz();
+	}
+
+	return 0;
+}
+
+double JsonExport::GetSnrObservationIfExist(ObsData obs, ObservationBand band)
+{
+	if (obs.SnrObservations().find(band) != obs.SnrObservations().end())
+	{
+		return obs.SnrObservations().at(band).SNR();
+	}
+
+	return 0;
+}
+
+
+SatelliteObservation JsonExport::CreateSatObservation(Satellite sv, ObsData obs, const Ephemeris& ephemeris)
 {
 	SatelliteObservation satObs;
 	satObs.SatelliteSystem = sv.SvString();
 		
-	satObs.ECEF_Position = ephemeris->Position_E();
-	satObs.ClockOffset = ephemeris->SatelliteClockError__s();
-	satObs.ClockDrift = ephemeris->SatelliteClockDrift__1Ds();
-	satObs.RelativisticError = ephemeris->RelativisticError__s();
+	satObs.ECEF_Position = ephemeris.Position_E();
+	satObs.ClockOffset = ephemeris.SatelliteClockError__s();
+	satObs.ClockDrift = ephemeris.SatelliteClockDrift__1Ds();
+	satObs.RelativisticError = ephemeris.RelativisticError__s();
 
 	satObs.Code = GenericObservation();
-	satObs.Code.Band_1 = obs.CodeObservations().at(ObservationBand::Band_1).Pseudorange__m();
-	satObs.Code.Band_2 = obs.CodeObservations().at(ObservationBand::Band_2).Pseudorange__m();
-	satObs.Code.Band_5 = obs.CodeObservations().at(ObservationBand::Band_5).Pseudorange__m();
+	satObs.Code.Band_1 = this->GetCodeObservationIfExist(obs, ObservationBand::Band_1);
+	satObs.Code.Band_2 = this->GetCodeObservationIfExist(obs, ObservationBand::Band_2);
+	satObs.Code.Band_5 = this->GetCodeObservationIfExist(obs, ObservationBand::Band_5);
 
 	satObs.Carrier = GenericObservation();
-	satObs.Carrier.Band_1 = obs.PhaseObservations().at(ObservationBand::Band_1).Carrierphase__Cycles();
-	satObs.Carrier.Band_2 = obs.PhaseObservations().at(ObservationBand::Band_2).Carrierphase__Cycles();
-	satObs.Carrier.Band_5 = obs.PhaseObservations().at(ObservationBand::Band_5).Carrierphase__Cycles();
+	satObs.Carrier.Band_1 = this->GetPhaseObservationIfExist(obs, ObservationBand::Band_1);
+	satObs.Carrier.Band_2 = this->GetPhaseObservationIfExist(obs, ObservationBand::Band_2);
+	satObs.Carrier.Band_5 = this->GetPhaseObservationIfExist(obs, ObservationBand::Band_5);
 
 	satObs.Doppler = GenericObservation();
-	satObs.Doppler.Band_1 = obs.DopplerObservations().at(ObservationBand::Band_1).Doppler__Hz();
-	satObs.Doppler.Band_2 = obs.DopplerObservations().at(ObservationBand::Band_2).Doppler__Hz();
-	satObs.Doppler.Band_5 = obs.DopplerObservations().at(ObservationBand::Band_5).Doppler__Hz();
+	satObs.Doppler.Band_1 = this->GetDopplerObservationIfExist(obs, ObservationBand::Band_1);
+	satObs.Doppler.Band_2 = this->GetDopplerObservationIfExist(obs, ObservationBand::Band_2);
+	satObs.Doppler.Band_5 = this->GetDopplerObservationIfExist(obs, ObservationBand::Band_5);
 
 	satObs.Snr = GenericObservation();
-	satObs.Snr.Band_1 = obs.SnrObservations().at(ObservationBand::Band_1).SNR();
-	satObs.Snr.Band_2 = obs.SnrObservations().at(ObservationBand::Band_2).SNR();
-	satObs.Snr.Band_5 = obs.SnrObservations().at(ObservationBand::Band_5).SNR();
+	satObs.Snr.Band_1 = this->GetSnrObservationIfExist(obs, ObservationBand::Band_1);
+	satObs.Snr.Band_2 = this->GetSnrObservationIfExist(obs, ObservationBand::Band_2);
+	satObs.Snr.Band_5 = this->GetSnrObservationIfExist(obs, ObservationBand::Band_5);
 
 	return satObs;
 }
@@ -46,8 +87,8 @@ void JsonExport::Export(std::vector<Satellite>& satellites, std::string path)
 		for (int i = 0; i < sv.ObservationData().size(); i++)
 		{
 			auto obs = sv.ObservationData().at(i);			
-			const auto& ephemeris = sv.SatelliteEphemeris().at(ObservationBand::Band_1).at(i).get();
-			SatelliteObservation satObs = CreateSatObservation(sv, obs, ephemeris);
+			const auto& ephemeris = *sv.SatelliteEphemeris().at(ObservationBand::Band_1).at(i).get();
+			SatelliteObservation satObs = this->CreateSatObservation(sv, obs, ephemeris);
 
 			// if data empty, insert - else iterate 
 			if (this->_SatelliteData.empty())
--- OpenRTK/Export/JsonExport.hpp
+++ OpenRTK/Export/JsonExport.hpp
@@ -12,7 +12,11 @@ class JsonExport
 private:
 	std::vector<SatelliteData> _SatelliteData;
 
-	SatelliteObservation CreateSatObservation(Satellite sv, ObsData obs, const Ephemeris* ephemeris);
+	SatelliteObservation CreateSatObservation(Satellite sv, ObsData obs, const Ephemeris& ephemeris);
+	double GetCodeObservationIfExist(ObsData obs, ObservationBand band);
+	double GetPhaseObservationIfExist(ObsData obs, ObservationBand band);
+	double GetDopplerObservationIfExist(ObsData obs, ObservationBand band);
+	double GetSnrObservationIfExist(ObsData obs, ObservationBand band);
 
 public:	
 	void Export(std::vector<Satellite>& satellites, std::string path);
