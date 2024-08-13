14910b1ca0ace3e506b42cce417a19b50830332a
aarendt <andi.arendt@icloud.com>
2024-08-13 22:09:45 +0200

    dummy scripts - nonworking

--- Analysis/PPP/PPP_LSQ_single_phase.m
+++ Analysis/PPP/PPP_LSQ_single_phase.m
@@ -1,5 +1,5 @@
-clear; 
-init;
+% clear; 
+% init;
 
 idx_E    = ([S.SvSystemID_t_eph] >= 17664) & ([S.SvSystemID_t_eph] <= 17664+255); %17664 is 69 ('E') << 8
 SvId_E   = unique(S.SvSystemID_t_eph(idx_E));
@@ -7,48 +7,26 @@ number_E = numel(unique(S.SvSystemID_t_eph(idx_E)));
 
 ctr = 1;
 
-idx_Galileo = cellfun(@(x)strcmp(x(1), 'E'), S.SvSystem_t_eph);
-idx_GPS     = cellfun(@(x)strcmp(x(1), 'G'), S.SvSystem_t_eph) .* 0;
+idx_Galileo = cellfun(@(x)strcmp(x(1), 'E'), S.SvSystem_t_eph).*0;
+idx_GPS     = cellfun(@(x)strcmp(x(1), 'G'), S.SvSystem_t_eph);
 
 idx_SV = idx_Galileo | idx_GPS;
 
 svID = unique(S.SvSystemID_t_eph(idx_SV)); 
 nAmbiguity = numel(svID);
-
-% KF Initialization
 nStates = 5 + nAmbiguity;
-F = eye(nStates);
-% F(1:3, 4:6) = eye(3) .* 30; % I * dt
 
-Q_pos = [0.01 0.01 0.01] ./ 1e3;
-% Q_vel = [0.01 0.01 0.01];
-Q_cdt = 1e-3;
-Q_ztd = 1e-9;
-Q_N   = ones(1, nAmbiguity) .* 1e-7; % Ambiguities
+POS    = station_pos__m;
+CDTR   = 0;
+ZTD    = 0;
+AMBIG  = zeros(nAmbiguity,1);
 
-Q = diag([Q_pos, Q_cdt, Q_ztd, Q_N]);
-kf = Kalman.Kalman(F, Q);
-kf.SetInitialStates([station_pos__m'; 0; 0; zeros(nAmbiguity,1)]);    % ECEF Position [m]
-                
-pos = station_pos__m;
-cdr = 0;
-zdt = 0;
-Ambig = zeros(1,nAmbiguity);
+x_hat = [POS';CDTR;ZTD;AMBIG];
 
-POS   = station_pos__m;
-% VEL   = kf.X(4:6);
-CDTR  = 0;
-ZTD   = 0;
-AMBIG = zeros(nAmbiguity,1);
-
-P = eye(nStates);
-
-P_pri  = eye(nStates);
-P_post = eye(nStates);
-X_pri  = kf.X.';
-X_post = kf.X.';
-
-KfIterations = 1;
+pos(1,:)   = POS;
+cdtr(1)    = CDTR;
+ztd(1)     = ZTD;
+ambig(1,:) = NaN(1,nAmbiguity);
 
 timestamps = unique(S.ObsToc);
 for tt = timestamps.'   
@@ -59,59 +37,54 @@ for tt = timestamps.'
     
     % Indices
     code_idx   = idx & (idx_Galileo | idx_GPS) & ...
-                       (S.Code_1 > 1e3) & (S.Code_1 > 1e3) & ...                % valid Pseudoranges
-                       ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
+                       (S.Code_1 > 1e3) & (S.Code_2 > 1e3) & ...                % valid Pseudoranges
+                       ((S.Band == 1) | (S.Band == 2));                         % E1 and E5
     code_1_idx = code_idx & (S.Band == 1);
-    code_5_idx = code_idx & (S.Band == 5);
+    code_2_idx = code_idx & (S.Band == 2);
     
     phase_idx   = idx & (idx_Galileo | idx_GPS) & ...
-                       (S.Phase_1 > 0) & (S.Code_5 > 0) & ...                   % valid Carrierphases
-                       ((S.Band == 1) | (S.Band == 5));                         % E1 and E5
+                       (S.Phase_1 > 0) & (S.Code_2 > 0) & ...                   % valid Carrierphases
+                       ((S.Band == 1) | (S.Band == 2));                         % E1 and E5
     phase_1_idx = phase_idx & (S.Band == 1);
-    phase_5_idx = phase_idx & (S.Band == 5);
+    phase_2_idx = phase_idx & (S.Band == 2);
 
-    [ia,ib] = ismember(S.SvSystemID_t_eph(code_1_idx), S.SvSystemID_t_eph(code_5_idx));
+    [ia,ib] = ismember(S.SvSystemID_t_eph(code_1_idx), S.SvSystemID_t_eph(code_2_idx));
     tmp = find(code_1_idx);
     code_1_idx(tmp(~ia)) = 0;
     
-    [ia,ib] = ismember(S.SvSystemID_t_eph(phase_1_idx), S.SvSystemID_t_eph(phase_5_idx));
+    [ia,ib] = ismember(S.SvSystemID_t_eph(phase_1_idx), S.SvSystemID_t_eph(phase_2_idx));
     tmp = find(phase_1_idx);
     phase_1_idx(tmp(~ia)) = 0;
 
     idx_Amb = ismember(svID, unique(S.SvSystemID_t_eph(phase_idx))); %index of Ambiguity data in State vector
 
-    % Propagation
-    kf.PredictIdx([ones(nStates-nAmbiguity,1);idx_Amb]);
-    % kf.Predict();
-
-    X_pri(ctr,:)   = kf.X.';
-    P_pri(:,:,ctr) = kf.P;
-
-    for ii = 1:KfIterations 
+    for ii = 1:5
         % Calc Distance 
         dist = Vector.EuclidianDistance_3D(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
                                            POS(1), POS(2), POS(3));
-
+    
         % Sagnac Correction
         sag = Transformation.CalcSagnac( [S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx)], ...
                                          [POS(1), POS(2), POS(3)]);
-
+    
         geo_dist = dist + sag;
-
+    
         % distance Vector & normalized distance Vector
         d = Vector.DistanceVector_3D(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
                                      POS(1), POS(2), POS(3));
-
+    
         e = Vector.NormalizedDistanceVector(S.x(code_1_idx), S.y(code_1_idx), S.z(code_1_idx), ...
                                             POS(1), POS(2), POS(3));
-
+    
         % SV Clock Error & Relativistic effects
         cdt_sv  = Transformation.SpeedOfLight__mDs * S.SvClockOffset(code_1_idx);    
         cdt_rel = Transformation.SpeedOfLight__mDs * S.RelativisticError(code_1_idx);
     
         % elevation mask
         elevation = Generic.CalcElevation(POS(1), POS(2), POS(3), d(:,1), d(:,2), d(:,3));
-                   
+            
+        idx_el = elevation > (15/180*pi) & elevation < (165/180*pi);    
+        
         % Troposphere
         [lat__rad, lon__rad, alt__m] = Transformation.ecef2wgs84(POS(1), POS(2), POS(3));
     
@@ -126,23 +99,12 @@ for tt = timestamps.'
         tropo_offset = (d_dry + d_wet) .* M_E;
                
         % Iono-Free LC
-        rho_iono_free = Generic.CalcIonoFreeLinearCombination(S.Code_1(code_1_idx), S.Code_5(code_5_idx), F_E1_Galileo__Hz, F_E5a_Galileo__Hz);
-        phi_iono_free = Generic.CalcIonoFreeLinearCombination(S.Phase_1(phase_1_idx), S.Phase_5(phase_5_idx), F_E1_Galileo__Hz, F_E5a_Galileo__Hz);
-     
-        idx_zero = AMBIG(idx_Amb) == 0;
-        if any(idx_zero)
-            tmp = find(idx_Amb);
-            delta = phi_iono_free - rho_iono_free;
-            AMBIG(tmp(idx_zero)) = delta(idx_zero);
-
-            xx = zeros(nAmbiguity, 1);
-            xx(tmp(idx_zero)) = 1;
-            kf.X(logical([zeros(nStates-nAmbiguity,1);xx])) = delta(idx_zero);
-        end
-
+        rho_iono_free = Generic.CalcIonoFreeLinearCombination(S.Code_1(code_1_idx), S.Code_2(code_2_idx), F_E1_Galileo__Hz, F_E5a_Galileo__Hz);
+        phi_iono_free = Generic.CalcIonoFreeLinearCombination(S.Phase_1(phase_1_idx), S.Phase_2(phase_2_idx), F_E1_Galileo__Hz, F_E5a_Galileo__Hz);
+    
         pseudorange_est  = rho_iono_free + cdt_sv + cdt_rel - CDTR - tropo_offset;            
         carrierphase_est = phi_iono_free + cdt_sv + cdt_rel - CDTR - tropo_offset - AMBIG(idx_Amb);
-
+    
         nObs = numel(e(:,1));
 
         % get number of active states (n + active Ambiguity)
@@ -157,88 +119,74 @@ for tt = timestamps.'
     
         H = [H_rho; H_phi];
          
-        dy = [ pseudorange_est  - geo_dist; ...
-               carrierphase_est - geo_dist];
+        y = [ pseudorange_est  - geo_dist; ...
+              carrierphase_est - geo_dist];
         
-        if ii < KfIterations
-            kf.CorrectIterativeEKFIdx(H, dy, eye(numel(dy)), [ones(nStates-nAmbiguity,1);idx_Amb]);
-        else            
-            kf.CorrectEKFIdx(H, dy, eye(numel(dy)), [ones(nStates-nAmbiguity,1);idx_Amb]);            
+        x_hat = (H.'*H)\H.'*y;
+
+        POS            = POS + x_hat(1:3)';                
+        CDTR           = CDTR + x_hat(4);
+        ZTD            = ZTD + x_hat(5);
+        AMBIG(idx_Amb) = AMBIG(idx_Amb) + x_hat(6:end); 
+
+        if all(x_hat < 1e-5)
+            break;
         end
-
-        POS   = kf.X(1:3)';                
-        CDTR  = kf.X(4);
-        ZTD   = kf.X(5);
-        A = kf.X(6:end);
-        AMBIG(idx_Amb) = A(idx_Amb); 
     end
-
-    pos(ctr,:)   = POS;
-    % vel(ctr,:)   = kf.X(4:6);
-    cdr(ctr,:)   = CDTR;
-    ztd(ctr,:)   = ZTD;
-    if ctr == 1
-        Ambig(ctr,:) = Ambig(ctr,:);
-    else
-        Ambig(ctr,:) = Ambig(ctr-1,:);
-    end
-    Ambig(ctr,idx_Amb) = AMBIG(idx_Amb)';
-    P(ctr,:) = diag(kf.P)';
-
-    % for RTS
-    X_post(ctr,:) = kf.X.';
-    P_post(:,:,ctr) = kf.P;
 
     ctr = ctr+1;
+    pos(ctr,:) = POS;
+    cdtr(ctr) = CDTR;
+    ztd(ctr) = ZTD;
+    ambig(ctr,:) = NaN(1,nAmbiguity);
+    ambig(ctr,idx_Amb) = AMBIG(idx_Amb);
 end
-         
-% ReadRtkPos;
-% return;
+
 %%
 afigure(42);
 subplot(4,2,1);
 hold on; grid on;
 yline(station_pos__m(1), 'Color', Color.BLACK, 'HandleVisibility','off')
 plot(pos(:,1), 'DisplayName', 'EKF (5 States)')
-% ylim([station_pos__m(1)-50,  station_pos__m(1)+50])
+ylim([station_pos__m(1)-50,  station_pos__m(1)+50])
 legend('show')
 
 subplot(4,2,2);
 hold on; grid on;
 plot(station_pos__m(1) - pos(:,1), 'DisplayName', 'EKF (5 States)')
-% ylim([-20 20])
+ylim([-20 20])
 legend('show')
 
 subplot(4,2,3);
 hold on; grid on;
 yline(station_pos__m(2), 'Color', Color.BLACK, 'HandleVisibility','off')
 plot(pos(:,2), 'DisplayName', 'EKF (5 States)')
-% ylim([station_pos__m(2)-50, station_pos__m(2)+50])
+ylim([station_pos__m(2)-50, station_pos__m(2)+50])
 legend('show')
 
 subplot(4,2,4);
 hold on; grid on;
 plot(station_pos__m(2) - pos(:,2), 'DisplayName', 'EKF (5 States)')
-% ylim([-20 20])
+ylim([-20 20])
 legend('show')
 
 subplot(4,2,5);
 hold on; grid on;
 yline(station_pos__m(3), 'Color', Color.BLACK, 'HandleVisibility','off')
 plot(pos(:,3), 'DisplayName', 'EKF (5 States)')
-% ylim([station_pos__m(3)-50, station_pos__m(3)+50])
+ylim([station_pos__m(3)-50, station_pos__m(3)+50])
 legend('show')
 
 subplot(4,2,6);
 hold on; grid on;
 plot(station_pos__m(3) - pos(:,3), 'DisplayName', 'EKF (5 States)')
-% ylim([-20 20])
+ylim([-20 20])
 legend('show')
 
 subplot(4,2,7);
 hold on; grid on;
 title('dt_r')
-plot(cdr ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'EKF (5 States)')
+plot(cdtr ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'EKF (5 States)')
 legend('show')
 
 subplot(4,2,8);
