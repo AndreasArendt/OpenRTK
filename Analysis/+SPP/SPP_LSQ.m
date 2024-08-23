555264c966868a0ca3fa8d4aa272126c207e7276
aarendt <andi.arendt@icloud.com>
2024-08-23 22:05:40 +0200

    cleanup spp lsq algorithm

--- Analysis/+SPP/SPP_LSQ.m
+++ Analysis/+SPP/SPP_LSQ.m
@@ -1,7 +1,7 @@
 LoadData;
 
-pos = [0,0,0];
-cdtr = 0;
+pos_E__m           = zeros(numel(SatelliteData), 3);
+rx_clock_offset__m = zeros(numel(SatelliteData), 1);
 
 posix = NaN(size(SatelliteData));
 
@@ -26,8 +26,8 @@ for ii = 1:numel(SatelliteData)
     idx = idx_gps & idx_code_1 & idx_code_2 & idx_healthy;
 
     %% Algorithm    
-    POS  = pos(ii,:);
-    CDTR = cdtr(ii);
+    POS_E__M           = pos_E__m(ii,:);
+    RX_CLOCK_OFFSET__M = rx_clock_offset__m(ii);
     
     ECEF_Position = [Observations.ECEF_Position];
     ECEF_x = [ECEF_Position(idx).x]';
@@ -36,37 +36,34 @@ for ii = 1:numel(SatelliteData)
 
     for jj = 1:10           
         % Calc Distance 
-        dist = Vector.EuclidianDistance_3D(ECEF_x, ECEF_y, ECEF_z, POS(1), POS(2), POS(3));
+        dist = Vector.EuclidianDistance_3D(ECEF_x, ECEF_y, ECEF_z, POS_E__M(1), POS_E__M(2), POS_E__M(3));
 
         % Sagnac Correction
-        sag = Transformation.CalcSagnac( [ECEF_x, ECEF_y, ECEF_z], [POS(1), POS(2), POS(3)]);
+        sag = Transformation.CalcSagnac( [ECEF_x, ECEF_y, ECEF_z], [POS_E__M(1), POS_E__M(2), POS_E__M(3)]);
 
         % correct geographic distance of earth rotation
         geo_dist = dist + sag;
         
-        cdt_sv  = Transformation.SpeedOfLight__mDs * [Observations(idx).ClockOffset]';    
-        cdt_rel = Transformation.SpeedOfLight__mDs * [Observations(idx).RelativisticError]';
+        sv_clock_offset__m = Transformation.SpeedOfLight__mDs * [Observations(idx).ClockOffset]';    
+        sv_relativistic__m = Transformation.SpeedOfLight__mDs * [Observations(idx).RelativisticError]';
     
         % elevation mask
-        elevation = Generic.CalcElevation(POS(1), POS(2), POS(3), ECEF_x, ECEF_y, ECEF_z);
-
-        idx_el = elevation > (5/180*pi) & elevation < (175/180*pi);
-
+        elevation = Generic.CalcElevation(POS_E__M(1), POS_E__M(2), POS_E__M(3), ECEF_x, ECEF_y, ECEF_z);
+        
         % Troposphere Model
-        tropo_offset = Troposphere.CalcTropoOffset(POS(1), POS(2), POS(3), elevation);
+        tropo_offset = Troposphere.CalcTropoOffset(POS_E__M(1), POS_E__M(2), POS_E__M(3), elevation);
             
         % Iono-Free LC
         rho_iono_free = Generic.CalcIonoFreeLinearCombination([Code(idx).Band_1]', [Code(idx).Band_2]', F_E1_Galileo__Hz, F_L2_GPS__Hz);
                 
-        dist_est = rho_iono_free + cdt_sv + cdt_rel - CDTR - tropo_offset;        
+        e = Vector.NormalizedDistanceVector(ECEF_x, ECEF_y, ECEF_z, POS_E__M(1), POS_E__M(2), POS_E__M(3));    
+        A = [-e, ones(numel(e(:,1)),1)];                
         
-        e = Vector.NormalizedDistanceVector(ECEF_x, ECEF_y, ECEF_z, POS(1), POS(2), POS(3));
-    
-        A = [-e, ones(numel(e(:,1)),1)];
-                
+        dist_est = rho_iono_free + sv_clock_offset__m + sv_relativistic__m - RX_CLOCK_OFFSET__M - tropo_offset;        
         y = dist_est - geo_dist;
 
         % Apply elevation filter only if more than 4SVs available
+        idx_el = elevation > (5/180*pi) & elevation < (175/180*pi);
         if nnz(idx_el) > 4
             A = A(idx_el,:);
             y = y(idx_el);
@@ -74,16 +71,16 @@ for ii = 1:numel(SatelliteData)
 
         x_hat = (A.'*A)\A.'*y;
         
-        POS = POS + x_hat(1:3)';        
-        CDTR = CDTR + x_hat(4);
+        POS_E__M                = POS_E__M + x_hat(1:3)';        
+        RX_CLOCK_OFFSET__M = RX_CLOCK_OFFSET__M + x_hat(4);
 
         if all(abs(x_hat(1:3)) < 1e-3)
             break;
         end
     end
         
-    pos(ii+1,:) = POS;
-    cdtr(ii+1)  = CDTR;
+    pos_E__m(ii+1,:)          = POS_E__M;
+    rx_clock_offset__m(ii+1)  = RX_CLOCK_OFFSET__M;
 end
 
 %%
@@ -91,46 +88,46 @@ afigure(42);
 subplot(4,2,1);
 hold on; grid on;
 yline(station_pos__m(1), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos(:,1), 'DisplayName', 'LSQ (4 States)')
+plot(pos_E__m(:,1), 'DisplayName', 'LSQ (4 States)')
 ylim([station_pos__m(1)-500,  station_pos__m(1)+500])
 legend('show')
 
 subplot(4,2,2);
 hold on; grid on;
-plot(station_pos__m(1) - pos(:,1), 'DisplayName', 'LSQ (4 States)')
+plot(station_pos__m(1) - pos_E__m(:,1), 'DisplayName', 'LSQ (4 States)')
 ylim([-20 20])
 legend('show')
 
 subplot(4,2,3);
 hold on; grid on;
 yline(station_pos__m(2), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos(:,2), 'DisplayName', 'LSQ (4 States)')
+plot(pos_E__m(:,2), 'DisplayName', 'LSQ (4 States)')
 ylim([station_pos__m(2)-500, station_pos__m(2)+500])
 legend('show')
 
 subplot(4,2,4);
 hold on; grid on;
-plot(station_pos__m(2) - pos(:,2), 'DisplayName', 'LSQ (4 States)')
+plot(station_pos__m(2) - pos_E__m(:,2), 'DisplayName', 'LSQ (4 States)')
 ylim([-20 20])
 legend('show')
 
 subplot(4,2,5);
 hold on; grid on;
 yline(station_pos__m(3), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos(:,3), 'DisplayName', 'LSQ (4 States)')
+plot(pos_E__m(:,3), 'DisplayName', 'LSQ (4 States)')
 ylim([station_pos__m(3)-500, station_pos__m(3)+500])
 legend('show')
 
 subplot(4,2,6);
 hold on; grid on;
-plot(station_pos__m(3) - pos(:,3), 'DisplayName', 'LSQ (4 States)')
+plot(station_pos__m(3) - pos_E__m(:,3), 'DisplayName', 'LSQ (4 States)')
 ylim([-20 20])
 legend('show')
 
 subplot(4,2,7);
 hold on; grid on;
 title('dt_r')
-plot(cdtr ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'LSQ (4 States)')
+plot(rx_clock_offset__m ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,8);
