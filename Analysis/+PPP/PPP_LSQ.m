06d64c8bd255edc85d39cd2c8b30ed2d6620fdeb
aarendt <andi.arendt@icloud.com>
2024-12-27 17:19:19 +0100

    cleanup and start development on PPP

--- Analysis/+PPP/PPP_LSQ.m
+++ Analysis/+PPP/PPP_LSQ.m
@@ -1,10 +1,11 @@
-LoadData;
+% LoadData;
+
+usePreciseEph = false;
 
 refpos__m = [NaN, NaN, NaN];
 
-pos_E__m              = zeros(numel(SatelliteData), 3);
-rx_clock_offset__m    = zeros(numel(SatelliteData), 1);
-zenith_tropo_delay__m = zeros(numel(SatelliteData), 1);
+pos_E__m           = zeros(numel(SatelliteData), 3);
+rx_clock_offset__m = zeros(numel(SatelliteData), 1);
 
 for ii = 1:numel(SatelliteData)
     %% Pre-Processing
@@ -24,15 +25,23 @@ for ii = 1:numel(SatelliteData)
     idx = idx_gps & idx_code_1 & idx_code_2 & idx_healthy;
 
     %% Algorithm    
-    POS_E__M              = pos_E__m(ii,:);
-    RX_CLOCK_OFFSET__M    = rx_clock_offset__m(ii);
-    ZENITH_TROPO_DELAY__M = zenith_tropo_delay__m(ii);
-
+    POS_E__M           = pos_E__m(ii,:);
+    RX_CLOCK_OFFSET__M = rx_clock_offset__m(ii);
+    
     ECEF_Position = [Observations.ECEF_Position];
     ECEF_x = [ECEF_Position(idx).x]';
     ECEF_y = [ECEF_Position(idx).y]';
     ECEF_z = [ECEF_Position(idx).z]';
     
+    if usePreciseEph == true
+        % TODO: SV order is implicitly used here
+        d = Generic.GetPreciseEphemeris(PreciseEphemerisData, {Observations(idx).SatelliteSystem}, SatelliteData(ii).PosixEpochTime__s);
+        v = [d.entries('Struct').Value];
+        ECEF_x = [v.x].';
+        ECEF_y = [v.y].';
+        ECEF_z = [v.z].';
+    end
+
     for jj = 1:10           
         % Calc Distance 
         dist = Vector.EuclidianDistance_3D(ECEF_x, ECEF_y, ECEF_z, POS_E__M(1), POS_E__M(2), POS_E__M(3));
@@ -49,28 +58,18 @@ for ii = 1:numel(SatelliteData)
         % elevation mask
         elevation = Generic.CalcElevation(POS_E__M(1), POS_E__M(2), POS_E__M(3), ECEF_x, ECEF_y, ECEF_z);
         
-        % Troposphere model - wet component estimated
-        [~, ~, alt__m] = Transformation.ecef2wgs84(POS_E__M(1), POS_E__M(2), POS_E__M(3));
-
-        d_dry = 2.3 .* exp(-0.116e-3 .* alt__m);
-    
-        if isinf(d_dry)
-            d_dry = 1e9;
-        end
-          
-        d_wet = 0.1 + ZENITH_TROPO_DELAY__M;        
-        M_E = 1.001./sqrt(0.002001 + sin(elevation).^2);                        
-        tropo_offset = (d_dry + d_wet) .* M_E;
-
+        % Troposphere Model
+        tropo_offset = Troposphere.CalcTropoOffset(POS_E__M(1), POS_E__M(2), POS_E__M(3), elevation);
+            
         % Iono-Free LC
         rho_iono_free = Generic.CalcIonoFreeLinearCombination([Code(idx).Band_1]', [Code(idx).Band_2]', F_E1_Galileo__Hz, F_L2_GPS__Hz);
                 
-        e = Vector.NormalizedDistanceVector(ECEF_x, ECEF_y, ECEF_z, POS_E__M(1), POS_E__M(2), POS_E__M(3));                       
-        A = [-e, ones(numel(e(:,1)),1), M_E];
-       
+        e = Vector.NormalizedDistanceVector(ECEF_x, ECEF_y, ECEF_z, POS_E__M(1), POS_E__M(2), POS_E__M(3));    
+        A = [-e, ones(numel(e(:,1)),1)];                
+        
         dist_est = rho_iono_free + sv_clock_offset__m + sv_relativistic__m - RX_CLOCK_OFFSET__M - tropo_offset;        
         y = dist_est - geo_dist;
-       
+
         % Apply elevation filter only if more than 4SVs available
         idx_el = elevation > (5/180*pi) & elevation < (175/180*pi);
         if nnz(idx_el) > 4
@@ -79,70 +78,66 @@ for ii = 1:numel(SatelliteData)
         end 
 
         x_hat = (A.'*A)\A.'*y;
-                
+        
         % State update
-        POS_E__M              = POS_E__M + x_hat(1:3)';        
-        RX_CLOCK_OFFSET__M    = RX_CLOCK_OFFSET__M + x_hat(4);
-        ZENITH_TROPO_DELAY__M = ZENITH_TROPO_DELAY__M + x_hat(5);
+        POS_E__M           = POS_E__M + x_hat(1:3)';        
+        RX_CLOCK_OFFSET__M = RX_CLOCK_OFFSET__M + x_hat(4);
 
-        if all(abs(x_hat(1:3)) < 1e-5)
+        if all(abs(x_hat(1:3)) < 1e-3)
             break;
         end
     end
         
-    pos_E__m(ii+1,:)            = POS_E__M;
-    rx_clock_offset__m(ii+1)    = RX_CLOCK_OFFSET__M;
-    zenith_tropo_delay__m(ii+1) = ZENITH_TROPO_DELAY__M;
+    pos_E__m(ii+1,:)          = POS_E__M;
+    rx_clock_offset__m(ii+1)  = RX_CLOCK_OFFSET__M;
 end
 
 % adjust off-by-one index
-pos_E__m              = pos_E__m(2:end,:);
-rx_clock_offset__m    = rx_clock_offset__m(2:end);
-zenith_tropo_delay__m = zenith_tropo_delay__m(2:end);
+pos_E__m           = pos_E__m(2:end,:);
+rx_clock_offset__m = rx_clock_offset__m(2:end);
 
 %%
 afigure(42);
 subplot(4,2,1);
 hold on; grid on;
 yline(refpos__m(1), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos_E__m(:,1), 'DisplayName', 'LSQ (5 States)')
+plot(pos_E__m(:,1), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,2);
 hold on; grid on;
-plot(refpos__m(1) - pos_E__m(:,1), 'DisplayName', 'LSQ (5 States)')
+plot(refpos__m(1) - pos_E__m(:,1), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,3);
 hold on; grid on;
 yline(refpos__m(2), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos_E__m(:,2), 'DisplayName', 'LSQ (5 States)')
+plot(pos_E__m(:,2), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,4);
 hold on; grid on;
-plot(refpos__m(2) - pos_E__m(:,2), 'DisplayName', 'LSQ (5 States)')
+plot(refpos__m(2) - pos_E__m(:,2), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,5);
 hold on; grid on;
 yline(refpos__m(3), 'Color', Color.BLACK, 'HandleVisibility','off')
-plot(pos_E__m(:,3), 'DisplayName', 'LSQ (5 States)')
+plot(pos_E__m(:,3), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,6);
 hold on; grid on;
-plot(refpos__m(3) - pos_E__m(:,3), 'DisplayName', 'LSQ (5 States)')
+plot(refpos__m(3) - pos_E__m(:,3), 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,7);
 hold on; grid on;
 title('dt_r')
-plot(rx_clock_offset__m ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'LSQ (5 States)')
+plot(rx_clock_offset__m ./ Transformation.SpeedOfLight__mDs, 'DisplayName', 'LSQ (4 States)')
 legend('show')
 
 subplot(4,2,8);
 hold on; grid on;
-title('ZTD_w', 'DisplayName', 'LSQ (5 States)')
-plot(zenith_tropo_delay__m, 'DisplayName', 'LSQ (5 States)')
-legend('show')
+title('ZTD_w')
+plot(NaN, 'HandleVisibility','off')
--- Analysis/Plots/PreciseEphemerides/PreciseEphemerisLagrange.m
+++ Analysis/Plots/PreciseEphemerides/PreciseEphemerisLagrange.m
@@ -15,13 +15,24 @@ end
 
 t_peph = datetime([PreciseEphemerisData.PosixEpochTime__s], 'ConvertFrom', 'posix').';
 
+%% Prepare Broadcast Ephemeris
+beph = NaN(numel(SatelliteData), 3);
+for ii = 1:numel(SatelliteData)
+    idx = strcmp({SatelliteData(ii).Observations.SatelliteSystem}, sv);
+
+    bEph = SatelliteData(ii).Observations(idx).ECEF_Position;
+    beph(ii,:) = [bEph.x, bEph.y, bEph.z];
+end
+
+t_beph = datetime([SatelliteData.PosixEpochTime__s], 'ConvertFrom', 'posix').';
+
 %%
 % new 5Hz timestamp
-t_new = [t_peph(1):seconds(0.2):t_peph(1)+hours(5)].';
+t_new = t_beph;%[t_peph(1):seconds(0.2):t_peph(1)+hours(2)].';
 
 %%
 af = afigure;
-subplot(3,1,1);
+subplot(4,1,1);
 hold on; grid on;
 
 n_poly = [3 5 7 9];
@@ -52,17 +63,25 @@ for n = n_poly
 end
 
 plot(t_peph, peph(:,1), 'x', 'DisplayName', 'Precise Eph')
+plot(t_beph, beph(:,1), '.', 'DisplayName', 'Broadcast Eph');
 
-subplot(3,1,2);
+subplot(4,1,2);
 hold on; grid on;
 plot(t_new, x_interp(:,9) - x_interp(:,7), 'DisplayName', '9-7')
 plot(t_new, x_interp(:,9) - x_interp(:,5), 'DisplayName', '9-5')
 plot(t_new, x_interp(:,9) - x_interp(:,3), 'DisplayName', '9-3')
 
-subplot(3,1,3);
+subplot(4,1,3);
 hold on; grid on;
 plot(t_new, x_interp(:,7) - x_interp(:,5), 'DisplayName', '7-5')
 plot(t_new, x_interp(:,7) - x_interp(:,3), 'DisplayName', '5-3')
+
+subplot(4,1,4);
+hold on; grid on;
+plot(t_new, x_interp(:,9) - beph(:,1), 'DisplayName', '9 - beph')
+plot(t_new, x_interp(:,7) - beph(:,1), 'DisplayName', '7 - beph')
+plot(t_new, x_interp(:,5) - beph(:,1), 'DisplayName', '5 - beph')
+plot(t_new, x_interp(:,3) - beph(:,1), 'DisplayName', '3 - beph')
 
 af.showlegend();
 af.linkaxes('x');
