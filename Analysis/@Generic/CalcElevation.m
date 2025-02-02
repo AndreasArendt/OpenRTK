ab7d7d9dae6ef67aee60afda64531f8128320cad
aarendt <andi.arendt@icloud.com>
2025-02-02 16:38:13 +0100

    fix dimension bug

--- Analysis/@Generic/CalcElevation.m
+++ Analysis/@Generic/CalcElevation.m
@@ -7,6 +7,6 @@ function elevation__rad = CalcElevation(x_rx, y_rx, z_rx, x_sv, y_sv, z_sv)
                 cos(lat__rad) * sin(lon__rad), ...
                 sin(lat__rad)];
 
-    e_zenith_vec = repmat(e_zenith, length(e), 1);    
-    elevation__rad = asin(dot(e, e_zenith_vec, 2));
+    e_zenith_vec = repmat(e_zenith, numel(x_sv), 1);   
+    elevation__rad = asin(dot(e, e_zenith_vec, 2));  
 end
